1640bb6074eadb18d79730115f970c32512c902f
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2023-07-22 12:43:26 +0700

    chore: add fizzBuzz script draft

---
+++ js/fizz_buzz.js
@@ -0,0 +1,33 @@
+/**
+ * Create an array filled by "fizz", "buzz", or "fizz buzz" based on
+ * this requirement below (highest priority from above).
+ *
+ * Muliples of 4 or 7: "fizz buzz"
+ * Odd sequence: "fizz"
+ * Even sequence: "buzz"
+ *
+ * @param {number} sequence
+ */
+function fizzBuzz(sequence) {
+  return [];
+}
+
+/**
+ * Example of fizzBuzz result.
+ */
+const fizzBuzz = [
+  "fizz", // 1
+  "buzz", // 2
+  "fizz", // 3
+  "fizz buzz", // 4
+  "fizz", // 5
+  "buzz", // 6
+  "fizz buzz", // 7
+  "fizz buzz", // 8
+  "fizz", // 9
+  "buzz", // 10
+  "fizz", // 11
+  "fizz buzz", // 12
+  "fizz", // 13
+  "fizz buzz", // 14
+];
