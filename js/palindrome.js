a6dfc6ecca8810305902e341594b98ffdd1ac67f
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2023-07-24 17:31:56 +0700

    chore: implement module on palindrome js script

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,9 +1,32 @@
+import { parseString } from "./helper.js";
+
 /**
- * Determine whether the given value is a palindrome or not.
+ * Determine whether the given value is a palindrome or not using reverse way.
  *
  * @param {string} value
  */
-function isPalindrome(value) {
+function isPalindromeUsingReverse(value) {
+  // write your code here
+  return true;
+}
+
+/**
+ * Determine whether the given value is a palindrome or not using loop way.
+ *
+ * @param {string} value
+ */
+function isPalindromeUsingLoop(value) {
+  // write your code here
+  return true;
+}
+
+/**
+ * Determine whether the given value is a palindrome or not using recursive way.
+ *
+ * @param {string} value
+ * @param {number} index
+ */
+function isPalindromeUsingRecursive(value, index = 0) {
   // write your code here
   return true;
 }
@@ -15,9 +38,34 @@ function submitForm(event) {
   event.preventDefault();
 
   const word = event.target["word"].value;
-  const result = isPalindrome(word)
-    ? "Yes, this word is a palindrome."
-    : "No, this word is not a palindrome.";
+  const method = event.target["method"].value;
+
+  let result;
+
+  try {
+    let isPalindrome;
+
+    if (method === "reverse") {
+      isPalindrome = isPalindromeUsingReverse(word);
+    } else if (method === "loop") {
+      isPalindrome = isPalindromeUsingLoop(word);
+    } else if (method === "recursive") {
+      isPalindrome = isPalindromeUsingRecursive(word);
+    } else {
+      throw new Error("Method must be reverse, loop, or recursive.");
+    }
+
+    result = isPalindrome
+      ? "Yes, this word is a palindrome."
+      : "No, this word is not a palindrome.";
+  } catch (error) {
+    alert(error.message);
+    console.error(error);
+  }
 
   document.getElementById("result").textContent = result;
 }
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  submitForm(event);
+});
