5417409fa14bd6333dc8ab54848ccba3e854d044
Septianata Rizky Pratama <ian.rizkypratama@gmail.com>
2023-07-24 14:49:54 +0700

    chore: add loop and recursive way on factorial js script

--- js/factorial.js
+++ js/factorial.js
@@ -1,9 +1,19 @@
 /**
- * Count factorial number from the given "n" value.
+ * Count factorial number from the given "n" value using loop way.
  *
  * @param {number} n
  */
-function countFactorial(n) {
+function countFactorialUsingLoop(n) {
+  // write your code here
+  return n;
+}
+
+/**
+ * Count factorial number from the given "n" value using recursive way.
+ *
+ * @param {number} n
+ */
+function countFactorialUsingRecursive(n) {
   // write your code here
   return n;
 }
@@ -15,7 +25,22 @@ function submitForm(event) {
   event.preventDefault();
 
   const n = event.target["n"].value;
-  const result = countFactorial(n);
+  const method = event.target["method"].value;
+
+  let result;
+
+  try {
+    if (method === "loop") {
+      result = countFactorialUsingLoop(n);
+    } else if (method === "recursive") {
+      result = countFactorialUsingRecursive(n);
+    } else {
+      throw new Error("Method must be loop or recursive.");
+    }
+  } catch (error) {
+    alert(error.message);
+    console.error(error);
+  }
 
   document.getElementById("result").textContent = result;
 }
