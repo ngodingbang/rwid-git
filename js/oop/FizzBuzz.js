1e127c7b51226af3e4b5bde0a15e9a1771d23117
KingKyoTrue <tanadicoding@gmail.com>
2023-07-31 17:50:21 +0700

    finished challenge oop/FizzBuzz

--- js/oop/FizzBuzz.js
+++ js/oop/FizzBuzz.js
@@ -12,6 +12,21 @@ export class FizzBuzz {
   }
 
   // write your code here
+  generate() {
+    let result = [];
+
+    for (let index = 1; index <= this.sequence; index++) {
+      if (index % 4 === 0 || index % 7 === 0) {
+        result.push("fizz buzz");
+      } else if (index % 2 === 1) {
+        result.push("fizz");
+      } else if (index % 2 === 0) {
+        result.push("buzz");
+      }
+    }
+
+    return result;
+  }
 }
 
 document.getElementById("form").addEventListener("submit", function (event) {
@@ -20,7 +35,7 @@ document.getElementById("form").addEventListener("submit", function (event) {
   try {
     const sequence = event.target["sequence"].value;
 
-    const result = new FizzBuzz(sequence);
+    const result = new FizzBuzz(sequence).generate();
 
     document.getElementById("result").textContent = JSON.stringify(result);
   } catch (error) {
