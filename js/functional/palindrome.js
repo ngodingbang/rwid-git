020a8ceda08c5cbb720c47a72503beb8dc26bfde
KingKyoTrue <tanadicoding@gmail.com>
2023-07-31 17:25:15 +0700

    finished challenge functional/palindrome

--- js/functional/palindrome.js
+++ js/functional/palindrome.js
@@ -7,7 +7,16 @@ import { parseString } from "../helper.js";
  */
 function isPalindromeUsingReverse(value) {
   // write your code here
-  return true;
+  value = parseString(value);
+
+  // Remove non-alphanumeric characters and convert to lowercase
+  const cleanedValue = value.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  // Reverse the cleaned value using array methods
+  const reversedValue = cleanedValue.split("").reverse().join("");
+
+  // Check if the cleaned value and the reversed value are the same
+  return cleanedValue === reversedValue;
 }
 
 /**
@@ -17,7 +26,22 @@ function isPalindromeUsingReverse(value) {
  */
 function isPalindromeUsingLoop(value) {
   // write your code here
-  return true;
+  value = parseString(value);
+
+  const cleanedValue = value.toLowerCase().replace(/[^a-z0-9]/g, "");
+
+  // Initialize length of cleanedValue
+  const length = cleanedValue.length;
+
+  // Loop through the characters to check for palindrome
+  for (let i = 0; i < length / 2; i++) {
+    // Check each position between the first and the last character
+    if (cleanedValue[i] !== cleanedValue[length - 1 - i]) {
+      return false; // If characters don't match, it's not a palindrome
+    }
+  }
+
+  return true; // All characters match, it's a palindrome
 }
 
 /**
@@ -28,7 +52,21 @@ function isPalindromeUsingLoop(value) {
  */
 function isPalindromeUsingRecursive(value, index = 0) {
   // write your code here
-  return true;
+  // Remove non-alphabetic characters from the value and convert it to lowercase
+  value = value.replace(/[^a-zA-Z]/g, "").toLowerCase();
+
+  // Base case: if the index reaches the middle of the string or beyond, the string is a palindrome
+  if (index >= Math.floor(value.length / 2)) {
+    return true;
+  }
+
+  // If the character at the first index and the last index are not the same, the string is not a palindrome
+  if (value[index] !== value[value.length - 1 - index]) {
+    return false;
+  }
+
+  // Recursively call the function with an incremented index to check the next character
+  return isPalindromeUsingRecursive(value, index + 1);
 }
 
 document.getElementById("form").addEventListener("submit", function (event) {
