d899dcc1f73fbff6fd937509341a1d567fb8b577
KingKyoTrue <tanadicoding@gmail.com>
2023-08-02 18:52:36 +0700

    finished update oop/Palindrome

--- js/oop/Palindrome.js
+++ js/oop/Palindrome.js
@@ -11,22 +11,25 @@ export class Palindrome {
     this.value = parseString(value);
   }
 
-  // write your code here
-  reverse() {
-    const cleanedValue = this.value.toLowerCase().replace(/[^a-z0-9]/g, "");
+  cleanString() {
+    this.value = this.value.toLowerCase().replace(/[^a-z0-9]/g, "");
+  }
 
+  checkPalindromeReverse() {
+    this.cleanString();
+    const cleanedValue = this.value;
     const reversedValue = cleanedValue.split("").reverse().join("");
 
     return cleanedValue === reversedValue;
   }
 
-  loop() {
-    const cleanedValue = this.value.toLowerCase().replace(/[^a-z0-9]/g, "");
-
+  checkPalindromeLoop() {
+    this.cleanString();
+    const cleanedValue = this.value;
     const length = cleanedValue.length;
 
-    for (let i = 0; i < length / 2; i++) {
-      if (cleanedValue[i] !== cleanedValue[length - 1 - i]) {
+    for (let index = 0; index < length / 2; index++) {
+      if (cleanedValue[index] !== cleanedValue[length - 1 - index]) {
         return false;
       }
     }
@@ -34,25 +37,15 @@ export class Palindrome {
     return true;
   }
 
-  recursive() {
-    this.value = this.value.replace(/[^a-zA-Z]/g, "").toLowerCase();
-  }
-
-  isPalindromeRecursive(index = 0) {
-    // Clean the string before checking for palindrome
-    this.recursive();
-
-    // Base case: if the index reaches the middle of the string or beyond, the string is a palindrome
+  checkPalindromeRecursive(index = 0) {
+    this.cleanString();
     if (index >= Math.floor(this.value.length / 2)) {
       return true;
     }
-
-    // If the character at the first index and the last index are not the same, the string is not a palindrome
     if (this.value[index] !== this.value[this.value.length - 1 - index]) {
       return false;
     }
 
-    // Recursive call to check the next pair of characters towards the middle
     return this.isPalindromeRecursive(index + 1);
   }
 }
@@ -64,21 +57,11 @@ document.getElementById("form").addEventListener("submit", function (event) {
     const word = event.target["word"].value;
     const method = event.target["method"].value;
 
-    let isPalindrome;
-
-    if (method === "reverse") {
-      isPalindrome = new Palindrome(word).reverse();
-    } else if (method === "loop") {
-      isPalindrome = new Palindrome(word).loop();
-    } else if (method === "recursive") {
-      isPalindrome = new Palindrome(word).recursive();
-    } else {
-      throw new Error("Method must be reverse, loop, or recursive.");
-    }
-
-    const result = isPalindrome
-      ? "Yes, this word is a palindrome."
-      : "No, this word is not a palindrome.";
+    const palindrome = new Palindrome(word);
+    const result =
+      palindrome.checkPalindromeReverse() ||
+      palindrome.checkPalindromeLoop() ||
+      palindrome.checkPalindromeRecursive();
 
     document.getElementById("result").textContent = result;
   } catch (error) {
