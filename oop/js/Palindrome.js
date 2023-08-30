883916f4aee173df7602358f58124d5dd870a033
Ngoding Bang <ngodingbang@gmail.com>
2023-08-31 00:48:27 +0700

    feat: finish palindrome js using oop

--- oop/js/Palindrome.js
+++ oop/js/Palindrome.js
@@ -1,3 +1,5 @@
+import { parseString } from "../helper.js";
+
 export class Palindrome {
   /** @type {string} */
   value;
@@ -9,7 +11,88 @@ export class Palindrome {
     this.value = parseString(value);
   }
 
-  // write your code here
+  /**
+   * Determine whether the given value is a palindrome or not.
+   *
+   * @param {"reverse" | "loop" | "recursive"} method
+   */
+  evaluate(method) {
+    if (method !== "reverse" && method !== "loop" && method !== "recursive") {
+      throw new Error("Method must be reverse, loop, or recursive.");
+    }
+
+    return method === "reverse"
+      ? this.evaluateUsingReverse()
+      : method === "loop"
+      ? this.evaluateUsingLoop()
+      : this.evaluateUsingRecursive();
+  }
+
+  /**
+   * Return palindrome human-readable description.
+   *
+   * @param {"reverse" | "loop" | "recursive"} method
+   */
+  getDescription(method) {
+    return this.evaluate(method)
+      ? "Yes, this word is a palindrome."
+      : "No, this word is not a palindrome.";
+  }
+
+  /**
+   * Determine whether the given value is a palindrome or not using reverse way.
+   */
+  evaluateUsingReverse() {
+    let newValue = "";
+
+    for (let index = this.value.length - 1; index >= 0; index--) {
+      newValue += this.value[index];
+    }
+
+    return this.value === newValue;
+  }
+
+  /**
+   * Determine whether the given value is a palindrome or not using loop way.
+   */
+  evaluateUsingLoop() {
+    for (let index = 0; index < Math.floor(this.value.length / 2); index++) {
+      const lastCharacterIndex = this.value.length - (index + 1);
+
+      const firstCharacter = this.value[index];
+      const lastCharacter = this.value[lastCharacterIndex];
+
+      if (firstCharacter !== lastCharacter) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  /**
+   * Determine whether the given value is a palindrome or not using recursive way.
+   *
+   * @param {number | undefined} index
+   */
+  evaluateUsingRecursive(index = undefined) {
+    index ||= 0;
+
+    if (index < Math.floor(this.value.length / 2)) {
+      const lastCharacterIndex = this.value.length - (index + 1);
+
+      const firstCharacter = this.value[index];
+      const lastCharacter = this.value[lastCharacterIndex];
+
+      if (firstCharacter !== lastCharacter) {
+        return false;
+      }
+
+      return this.evaluateUsingRecursive(this.value, index + 1);
+    }
+
+    return true;
+  }
 }
 
 document.getElementById("form").addEventListener("submit", function (event) {
@@ -19,7 +102,7 @@ document.getElementById("form").addEventListener("submit", function (event) {
     const word = event.target["word"].value;
     const method = event.target["method"].value;
 
-    const result = new Palindrome(word);
+    const result = new Palindrome(word).getDescription(method);
 
     document.getElementById("result").textContent = result;
   } catch (error) {
