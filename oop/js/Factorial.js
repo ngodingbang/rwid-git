4854a8e3625450ff1f2bdfd52136dec0ddf8a6a8
Ngoding Bang <ngodingbang@gmail.com>
2023-08-31 00:48:14 +0700

    feat: finish factorial js using oop

--- oop/js/Factorial.js
+++ oop/js/Factorial.js
@@ -1,4 +1,4 @@
-import { parseNumber } from "../../js/helper.js";
+import { parseNumber } from "../helper.js";
 
 export class Factorial {
   /** @type {number} */
@@ -11,7 +11,51 @@ export class Factorial {
     this.n = parseNumber(n);
   }
 
-  // write your code here
+  /**
+   * Count factorial number from the given "n" value.
+   *
+   * @param {"loop" | "recursive"} method
+   */
+  count(method) {
+    if (method !== "loop" && method !== "recursive") {
+      throw new Error("Method must be loop or recursive.");
+    }
+
+    return method === "loop"
+      ? this.countUsingLoop()
+      : this.countUsingRecursive();
+  }
+
+  /**
+   * Count factorial number from the given "n" value using loop way.
+   *
+   * @returns {number}
+   */
+  countUsingLoop() {
+    let result = 1;
+
+    for (let index = this.n; index > 0; index--) {
+      result = result * index;
+    }
+
+    return result;
+  }
+
+  /**
+   * Count factorial number from the given "n" value using recursive way.
+   *
+   * @param {number | undefined} n
+   * @returns {number}
+   */
+  countUsingRecursive(n = undefined) {
+    n ||= this.n;
+
+    if (n < 2) {
+      return 1;
+    }
+
+    return n * this.countUsingRecursive(n - 1);
+  }
 }
 
 document.getElementById("form").addEventListener("submit", function (event) {
@@ -21,7 +65,7 @@ document.getElementById("form").addEventListener("submit", function (event) {
     const n = event.target["n"].value;
     const method = event.target["method"].value;
 
-    const result = new Factorial(n);
+    const result = new Factorial(n).count(method);
 
     document.getElementById("result").textContent = result;
   } catch (error) {
