99967905df1c62e01515121b1bfa8037eb8239a1
Ngoding Bang <ngodingbang@gmail.com>
2023-09-06 01:07:13 +0700

    feat: create javascript asynchronous fetch api page

---
+++ pages/asynchronous/fetch_api/generateMember.js
@@ -0,0 +1,1 @@
+export { member as m12 } from "../../modules/generateMember12.js";
---
+++ pages/asynchronous/fetch_api/generateTrElement.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} prefixTarget
+ * @param {{ index?: number; name?: string; github_url?: string; visibility?: string; created_at?: Date; }} data
+ */
+export function generateTrElement(data = {}, tbodyTarget = "repo-tbody") {
+  /** @type {HTMLTableRowElement} */
+  const row = document.getElementById(tbodyTarget).insertRow(-1);
+
+  row.insertCell(0).textContent = data?.index || "";
+  row.insertCell(1).textContent = data?.name || "";
+  row.insertCell(2).innerHTML = data?.github_url
+    ? `<a href=${data?.github_url}>${data?.github_url}</a>`
+    : "";
+  row.insertCell(3).textContent = data?.visibility;
+  row.insertCell(4).textContent = data?.created_at;
+}
