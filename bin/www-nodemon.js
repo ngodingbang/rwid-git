8be3d8f1b1f27a2dbfd6df91a46e6b3540a011dd
Ngoding Bang <ngodingbang@gmail.com>
2023-09-14 13:30:08 +0700

    feat!: reverse npm init and using express.js

--- bin/www-nodemon.js
+++
@@ -1,6 +0,0 @@
-import nodemon from "nodemon";
-import "../env.js";
-
-nodemon({ script: "./bin/www", ext: "js" })
-  .on("start", () => console.info(`Nodemon started at ${process.env.APP_URL}.`))
-  .on("quit", () => console.info("Killing nodemon."));
