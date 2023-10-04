81ca342e5d1591a72769c61533014a98ec90fed5
Jaya Mustika <jayamustika25@gmail.com>
2023-10-04 10:10:05 +0700

    feat: finish FizzBuzz.js using oop

--- pages/oop/js/FizzBuzz.js
+++ pages/oop/js/FizzBuzz.js
@@ -1,63 +1,43 @@
 import { parseNumber } from "../../js/helper.js";
 
-export class FizzBuzz {
-  /** @type {number} */
-  sequence;
-
+/**
+ * FizzBuzz class to generate and display a modified FizzBuzz sequence.
+ */
+class FizzBuzz {
   /**
-   * @param {number} sequence
+   * Initializes a new instance of the FizzBuzz class.
+   * @param {number} n 
    */
-  constructor(sequence) {
-    this.sequence = parseNumber(sequence);
+  constructor(n) {
+    this.n = n;
   }
 
   /**
-   * Create an array filled by "fizz", "buzz", or "fizz buzz" based on
-   * this requirement below (highest priority from above).
-   *
-   * Muliples of 4 or 7: "fizz buzz"
-   * Odd sequence: "fizz"
-   * Even sequence: "buzz"
+   * A generator function to yield a modified FizzBuzz sequence up to the specified number.
+   * @param {number} [index=this.n] 
+   * @yields {string} 
    */
-  generate() {
-    let result = [];
-
-    for (let index = 1; index <= this.sequence; index++) {
-      if (index % 4 === 0 || index % 7 === 0) {
-        result.push("fizz buzz");
-      } else if (index % 2 === 1) {
-        result.push("fizz");
-      } else if (index % 2 === 0) {
-        result.push("buzz");
-      }
+  *fizzBuzzGenerator(index = this.n) {
+    for (let i = 1; i < index + 1; i++) {
+      yield i % 4 === 0 || i % 7 === 0
+        ? `Iteration ${i} : Fizz Buzz`
+        : i % 2 === 0
+        ? `Iteration ${i} : Fizz`
+        : i % 2 === 1
+        ? `Iteration ${i} : Buzz`
+        : undefined;
     }
+  }
 
-    return result;
+  /**
+   * Displays the modified FizzBuzz sequence up to the number specified during class instantiation in the console.
+   */
+  showAtConsole() {
+    for (const x of this.fizzBuzzGenerator()) {
+      console.info(x);
+    }
   }
 }
 
-document.getElementById("form").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  try {
-    const sequence = event.target["sequence"].value;
-
-    const fizzBuzzs = new FizzBuzz(sequence).generate();
-
-    document.getElementById("result").innerHTML = fizzBuzzs
-      .map(
-        (fizzBuzz, index) =>
-          `<div class="result-item-fizzbuzz bg-${
-            {
-              "fizz buzz": "red",
-              fizz: "blue",
-              buzz: "green",
-            }[fizzBuzz]
-          }">${index + 1} ${fizzBuzz}</div>`,
-      )
-      .join("");
-  } catch (error) {
-    alert(error.message);
-    console.error(error);
-  }
-});
+const FizzBuzz5 = new FizzBuzz(7);
+FizzBuzz5.showAtConsole();
