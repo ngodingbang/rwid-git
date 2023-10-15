59c148bced9b4ed9872727317aa095bc09c269cc
Ngoding Bang <ngodingbang@gmail.com>
2023-10-15 17:54:55 +0800

    feat: using winston to handle http and error logging

--- error-handler.js
+++ error-handler.js
@@ -1,4 +1,5 @@
 import createError from "http-errors";
+import { winston } from "./logger.js";
 
 /**
  * @param {Error} err
@@ -11,6 +12,10 @@ import createError from "http-errors";
 export function errorHandler(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  if (500 <= err.status && err.status < 600) {
+    winston.error(err);
+  }
 
   return res.status(err.status || 500).render("error");
 }
